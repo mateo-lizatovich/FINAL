79f9e53673575c4e937480af0cecca689fcbe90a
mateo-lizatovich <matee.lizatovic@gmail.com>
2022-07-22 00:01:28 -0300

    cambie color de los bordes e hice auto.html

--- Autos/AutosApp/views.py
+++ Autos/AutosApp/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 
-#def index(request):
-    #autos = Auto.objects.all()
-   # ctx = {'autos': autos}
+def index(request):
+    autos = Auto.objects.all()
+    ctx = {'autos': autos}
 
-   # return render(request,"AutosApp/index.html", ctx)
+    return render(request,"AutosApp/index.html", ctx)
    
    
 def autos(request):
-    return render(request,'AutosApp/autos.html',{})
+    autos = Auto.objects.all()
+    return render(request,'AutosApp/autos.html',{'autos': autos})
 
 def vendedores(request):
     return HttpResponse('vendedores')
