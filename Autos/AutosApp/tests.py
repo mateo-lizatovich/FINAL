f03e6ad6f383119b3c0910a298814ea81404db25
mateo-lizatovich <matee.lizatovic@gmail.com>
2022-07-25 22:18:32 -0300

    mas o menos terminado. FIN

--- Autos/AutosApp/tests.py
+++ Autos/AutosApp/tests.py
@@ -1,3 +1,13 @@
+from enum import auto
 from django.test import TestCase
-
+from .models import Auto
 # Create your tests here.
+
+
+class AutoTest(TestCase):
+    
+    def setUp(self):
+        Auto.objects.create(marca="BMW", modelo= "Q3", año ="20172312241", precio = "50000")
+    def test_auto_marca(self):
+        auto = Auto.objects.get(modelo = "Q3")
+        self.assertEqual(auto.marca, "BMW")
